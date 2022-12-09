7bec1c99eadf492a0057a16b642e2b326da4511d
diwash5 <diwashsubediiii@gmail.com>
2022-12-10 01:04:53 +0545

    Added send to url module and some symlink notes for later

--- bandwidthd-shell.sh
+++ bandwidthd-shell.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
+#Tested and most of it works. anything with uci doesn't work . Didnot test modifyprogramconfig though
 
-#Tested and works . Didnot test modifyprogramconfig though
-
+WEBSITEAPI="http://192.168.39.155:3600/input/"
 
 # this module configures the bandwidthd , only meant to be used for the first time
 modifyprogramconfig () {
@@ -21,20 +21,21 @@ PROGRAMINTERVAl=$( uci get bandwidthd.@bandwidthd[0].meta_refresh )
 SCRIPTINTERVAL=$((PROGRAMINTERVAl + 60))
 # Main Program begins
 
-# cleaning the data as required
+#--------------- cleaning the data as required-------------------------------
 
 # cleaning the data as recieved from bandwidthd
 # keeping the NA in awk so that i can replace things later on without messing up the column
-
 
 cat /log.1.*.cdf | grep -v  0.0.0.0 | awk -F "," '{print $1, "NA", "NA" ,$2,$3,$10}' > /tmp/cleanlog
 
 # Making a Good & Combined Dhcp file
 ( awk '{print $3,$4,$2}' /tmp/dhcp.leases && tail +2 /tmp/hosts/dhcp.cfg* | awk '{print $1,$2,"NA"}' ) > /tmp/cleandhcp
 
-
+#--------------------------Deleting The File----------------------------------- 
 # Now that i have my data , i can delete the log files from the drive to save storage and have accurate data for the next time
 # I'm deleting as i dont wanna compare if i have sent it before
+
+#rm /log.*
 
 
 #Replacing the IP with IP,Name & MAC
@@ -45,9 +46,21 @@ sed -i 's/'"$USERIP"' NA NA/'"$line"'/g' /tmp/cleanlog
 done < /tmp/cleandhcp
 
 
-# Making a Json Now and outputing it to terminal
+# Making a Json Now by reading each line of cleanlog file and sending the json to the url
+
 while read line
 do
-echo "$line" | awk '{printf "{ \"ip\" : \""$1"\" , \"name\" : \""$2"\" , \"macaddress\" : \""$3"\" , \"date\" : \""$4"\" , \"sent\" : \""$5"\" , \"recieved\" : \""$6"\"  }"}'
+SENDME=$(echo "$line" | awk '{printf "{ \"ip\" : \""$1"\" , \"deviceName\" : \""$2"\" , \"macaddress\" : \""$3"\" , \"date\" : \""$4"\" , \"upload\" : \""$5"\" , \"download\" : \""$6"\"  }"}')
+RESPONSE=$( curl -s --insecure --location --request POST "$WEBSITEAPI" \
+                    --header 'Content-Type: application/json' \
+                    --data-raw "$SENDME" )
+echo $RESPONSE
 sleep 1s
 done < /tmp/cleanlog
+
+
+## These are the symlink that needs to be created before doing anything
+## ln -s /tmp/log.1.0.cdf /log.1.0.cdf
+## ln -s /tmp/log.2.0.cdf /log.2.0.cdf
+## ln -s /tmp/log.3.0.cdf /log.3.0.cdf
+## ln -s /tmp/log.4.0.cdf /log.4.0.cdf
