1474eeecfa41af889d75dc07d7bb1754f9e3b245
diwash5 <diwashsubediiii@gmail.com>
2022-12-10 12:32:49 +0545

    Added the bigger loop for hourly, added some comments

--- bandwidthd-shell.sh
+++ bandwidthd-shell.sh
@@ -1,9 +1,7 @@
 #! /bin/sh
 
 #Tested and most of it works. anything with uci doesn't work . Didnot test modifyprogramconfig though
-
 WEBSITEAPI="http://192.168.39.155:3600/input/"
-
 # this module configures the bandwidthd , only meant to be used for the first time
 modifyprogramconfig () {
     # setting subset i.e your ip range, obtaining it from uci ; Mine is 192.168.39.1
@@ -11,32 +9,27 @@ modifyprogramconfig () {
     uci set bandwidthd.@bandwidthd[0].subnets="$MYSUBSET"
     # setting the output file of bandwidthd
     uci set bandwidthd.@bandwidthd[0].output_cdf=true
-
 }
-
-
 ## Getting the program refresh retrival
 ## and setting the script runnung interval to 1 minute after that so that the program is done outputing . This is not implemented yet
-PROGRAMINTERVAl=$( uci get bandwidthd.@bandwidthd[0].meta_refresh )
-SCRIPTINTERVAL=$((PROGRAMINTERVAl + 60))
+
+#### Making a loop that the main program resides in and 
+while :
+do
+sleep 3690s
+
 # Main Program begins
-
 #--------------- cleaning the data as required-------------------------------
-
 # cleaning the data as recieved from bandwidthd
 # keeping the NA in awk so that i can replace things later on without messing up the column
-
-cat /log.1.*.cdf | grep -v  0.0.0.0 | awk -F "," '{print $1, "NA", "NA" ,$2,$3,$10}' > /tmp/cleanlog
-
+cat /tmp/log.3.*.cdf | grep -v  0.0.0.0 | awk -F "," '{print $1, "NA", "NA" ,$2,$3,$10}' > /tmp/cleanlog
 # Making a Good & Combined Dhcp file
 ( awk '{print $3,$4,$2}' /tmp/dhcp.leases && tail +2 /tmp/hosts/dhcp.cfg* | awk '{print $1,$2,"NA"}' ) > /tmp/cleandhcp
-
 #--------------------------Deleting The File----------------------------------- 
 # Now that i have my data , i can delete the log files from the drive to save storage and have accurate data for the next time
 # I'm deleting as i dont wanna compare if i have sent it before
 
-#rm /log.*
-
+#rm /tmp/log.*
 
 #Replacing the IP with IP,Name & MAC
 while read line
@@ -44,22 +37,23 @@ do
 USERIP=$( echo "$line" | awk '{print $1}' )
 sed -i 's/'"$USERIP"' NA NA/'"$line"'/g' /tmp/cleanlog
 done < /tmp/cleandhcp
-
-
 # Making a Json Now by reading each line of cleanlog file and sending the json to the url
-
 while read line
 do
 SENDME=$(echo "$line" | awk '{printf "{ \"ip\" : \""$1"\" , \"deviceName\" : \""$2"\" , \"macaddress\" : \""$3"\" , \"date\" : \""$4"\" , \"upload\" : \""$5"\" , \"download\" : \""$6"\"  }"}')
 RESPONSE=$( curl -s --insecure --location --request POST "$WEBSITEAPI" \
                     --header 'Content-Type: application/json' \
                     --data-raw "$SENDME" )
-echo $RESPONSE
 sleep 1s
 done < /tmp/cleanlog
-
+logger "Data is sent to the server and local file is deleted"
+done
 
 ## These are the symlink that needs to be created before doing anything
+## Will implement a module later on that will add these symerlink if they don't exists
+## The main reason for this is to reduce storage needs and prevent flash from wearing down 
+## So placing them in RAM 
+ 
 ## ln -s /tmp/log.1.0.cdf /log.1.0.cdf
 ## ln -s /tmp/log.2.0.cdf /log.2.0.cdf
 ## ln -s /tmp/log.3.0.cdf /log.3.0.cdf
